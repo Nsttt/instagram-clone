f7cbb1863e11f0e684f93bea98ede0804b432930
Nestor <elnestinatorHD@gmail.com>
2021-03-21 12:49:22 +0100

    feat: new routes & context provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
 
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const SignUp = lazy(() => import('./pages/signup'));
 const Login = lazy(() => import('./pages/login'));
+const NotFound = lazy(() => import('./pages/notfound'));
 
-function App() {
+export default function App() {
+  const { user } = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
-
-export default App;
