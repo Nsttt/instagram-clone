921aace03a8426f1a75d6dc9ffd666ee7c3ca947
Nestor <elnestinatorHD@gmail.com>
2021-03-21 12:48:58 +0100

    fix: login page values for inputs

--- src/pages/login.js
+++ src/pages/login.js
@@ -49,6 +49,7 @@ export default function Login() {
                 placeholder="Email address"
                 className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
                 onChange={({ target }) => setEmailAddress(target.value)}
+                value={emailAddress}
               />
               <input
                 aria-label="Enter your password"
@@ -56,6 +57,7 @@ export default function Login() {
                 placeholder="Password"
                 className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
                 onChange={({ target }) => setPassword(target.value)}
+                value={password}
               />
               <button
                 disabled={isInvalid}
