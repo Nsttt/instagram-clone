06a5fa670b253426197b969cb3c678817bb15e69
Nestor <elnestinatorHD@gmail.com>
2021-03-22 12:54:12 +0100

    feat: dashboard page

---
+++ src/pages/dashboard.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import Header from '../components/header';
+import Sidebar from '../components/sidebar';
+import Timeline from '../components/timeline';
+
+export default function Dashboard() {
+  useEffect(() => {
+    document.title = 'Photosharing';
+  }, []);
+
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+        <Timeline />
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
