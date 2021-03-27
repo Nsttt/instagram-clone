cf251008ffc93332b776c02d64994b42eaf867c0
Nestor <elnestinatorHD@gmail.com>
2021-03-27 13:40:05 +0100

    feat: timeline component

---
+++ src/components/timeline.js
@@ -0,0 +1,20 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
+export default function Timeline() {
+  const { photos } = usePhotos();
+
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <>
+          <Skeleton count={4} width={640} height={500} className="mb-5" />
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
+    </div>
+  );
+}
