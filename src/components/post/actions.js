8d9145cf97e84eb1b550615d0beac98d77765ec9
Nestor <elnestinatorHD@gmail.com>
2021-04-06 19:43:57 +0200

    feat: post actions component
    Still WIP.

---
+++ src/components/post/actions.js
@@ -0,0 +1,30 @@
+import { useState, useContext } from 'react';
+import PropTypes, { checkPropTypes } from 'prop-types';
+import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
+
+export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) {
+  const {
+    user: { uid: userId = '' },
+  } = useContext(UserContext);
+
+  const [toggleLiked, setToggleLiked] = useState(likedPhoto);
+  const [likes, setLikes] = useState(totalLikes);
+  const { firebase, FieldValue } = useContext(FirebaseContext);
+
+  const handleToggleLiked = async () => {
+    setToggleLiked((toggleLiked) => !toggleLiked);
+
+    await firebase
+      .firestore()
+      .collection('photos')
+      .doc(docId)
+      .update({
+        likes: toggleLiked ? FieldValue.arrayRemove(userId) : FieldValue.arrayUnion(userId),
+      });
+
+    setLikes((likes) => (toggleLiked ? likes - 1 : likes + 1));
+  };
+
+  return <div />;
+}
