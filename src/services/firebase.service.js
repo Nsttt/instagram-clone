911cb558015ae8f669011cf0825c3d21f0d02667
Nestor <elnestinatorHD@gmail.com>
2021-04-10 19:55:47 +0200

    feat: new firebase call

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -10,6 +10,19 @@ export async function doesUsernameExists(username) {
   return result.docs.map((user) => user.data().length > 0);
 }
 
+export async function getUserByUsername(username) {
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('username', '==', username)
+    .get();
+
+  return result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+}
+
 export async function getUserByUserId(userId) {
   const result = await firebase.firestore().collection('users').where('userId', '==', userId).get();
 
