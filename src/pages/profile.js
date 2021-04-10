ba8cb056030c98e13891e97618ed9dca78bfae89
Nestor <elnestinatorHD@gmail.com>
2021-04-10 19:56:06 +0200

    feat: profile page WIP

---
+++ src/pages/profile.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { getUserByUsername } from '../services/firebase.service';
+import * as ROUTES from '../constants/routes';
+
+export default function Profile() {
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const [userExists, setUserExists] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const user = await getUserByUsername(username);
+      if (user.length > 0) {
+        setUser(user[0]);
+        setUserExists(true);
+      } else {
+        setUser(false);
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExists();
+  }, [username, history]);
+
+  return userExists ? (
+    <div className="bg-gray-background">
+      <div className="mx-auto max-w-screen-lg" />
+    </div>
+  ) : null;
+}
