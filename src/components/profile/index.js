f8b95d91627d334dfed9a72961a0fe5da27357f2
Nestor <elnestinatorHD@gmail.com>
2021-04-12 13:34:06 +0200

    feat: profile main component WIP

---
+++ src/components/profile/index.js
@@ -0,0 +1,52 @@
+import { useReducer, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+import { getUserPhotosByUsername } from '../../services/firebase.service';
+import Photos from './photos';
+
+export default function Profile({ user }) {
+  const reducer = (state, newState) => ({ ...state, ...newState });
+  const initialState = {
+    profile: {},
+    photosCollection: [],
+    followerCount: 0,
+  };
+
+  const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  useEffect(() => {
+    async function getProfileInfoAndPhotos() {
+      const photos = await getUserPhotosByUsername(user.username);
+      dispatch({ profile: user, photosCollection: photos, followerCount: user.followers.length });
+    }
+
+    getProfileInfoAndPhotos();
+  }, [user.username]);
+
+  return (
+    <>
+      <Header
+        photosCount={photosCollection ? photosCollection.length : 0}
+        profile={profile}
+        followerCount={followerCount}
+        setFollowerCount={dispatch}
+      />
+      <Photos photos={photosCollection} />
+    </>
+  );
+}
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    dateCreated: PropTypes.number.isRequired,
+    emailAddress: PropTypes.string.isRequired,
+    followers: PropTypes.array.isRequired,
+    following: PropTypes.array.isRequired,
+    fullName: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+};
