22e528500184c4492a70ec4f60b41652cb45ced7
Nestor <elnestinatorHD@gmail.com>
2021-04-16 15:58:22 +0200

    refactor: removed nested ternary & new context

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,20 +1,19 @@
+import { useContext } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import usePhotos from '../hooks/use-photos';
 import Post from './post';
+import LoggedInUserContext from '../context/logged-in-user';
 
 export default function Timeline() {
-  const { photos } = usePhotos();
+  const { user } = useContext(LoggedInUserContext);
+  const { photos } = usePhotos(user);
 
   return (
     <div className="container col-span-2">
       {!photos ? (
-        <>
-          <Skeleton count={4} width={640} height={500} className="mb-5" />
-        </>
-      ) : photos?.length > 0 ? (
-        photos.map((content) => <Post key={content.docId} content={content} />)
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
       ) : (
-        <p className="text-center text-2xl">Follow people to see photos!</p>
+        photos.map((content) => <Post key={content.docId} content={content} />)
       )}
     </div>
   );
