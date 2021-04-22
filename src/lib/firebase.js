1b5f2a527ba0fb8d375c6f67d6d29b6690dbc9b7
Nestor <elnestinatorHD@gmail.com>
2021-04-22 19:54:43 +0200

    feat: firebase lib file

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+// import seedDatabase from '../seed';
+
+const config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+//! Seed database function, only run once.
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
