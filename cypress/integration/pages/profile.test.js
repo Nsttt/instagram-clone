9d1011c98978ea1489152192f4e9faeeb192f1de
Nestor <elnestinatorHD@gmail.com>
2021-04-22 19:45:02 +0200

    test: cypress profile tests

---
+++ cypress/integration/pages/profile.test.js
@@ -0,0 +1,15 @@
+describe('Dashboard', () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.config().baseUrl}/p/raphael`);
+  });
+
+  it('goes to a profile page and validates the UI', () => {
+    cy.get('body').within(() => {
+      cy.get('div').should('contain.text', 'raphael');
+      cy.get('div').should('contain.text', 'Raffaello Sanzio da Urbino');
+      cy.get('div').should('contain.text', '5 photos');
+      cy.get('div').should('contain.text', '3 followers');
+      cy.get('div').should('contain.text', '0 following');
+    });
+  });
+});
